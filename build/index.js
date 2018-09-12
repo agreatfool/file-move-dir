e6bd9c231a1ca39ba8f86871400cbe3ad389f1ee
Jonathan Dai <nightghost5078@gmail.com>
2018-09-12 10:50:27 +0800

    Init ver

---
+++ build/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pkg = require('../package.json');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import * as LibFs from 'mz/fs';
+import * as LibPath from 'path';
+
+import * as program from 'commander';
+
+const pkg = require('../package.json');
+
